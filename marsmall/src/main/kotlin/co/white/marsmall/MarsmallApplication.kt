9f8a9bfc4e987c3c9d26bec51477c7190b60289a
Wordbe <seonghojin3@gmail.com>
2023-01-05 22:46:02 +0900

    init: springboot

---
+++ marsmall/src/main/kotlin/co/white/marsmall/MarsmallApplication.kt
@@ -0,0 +1,11 @@
+package co.white.marsmall
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarsmallApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarsmallApplication>(*args)
+}
---
+++ marsmall/src/test/kotlin/co/white/marsmall/MarsmallApplicationTests.kt
@@ -0,0 +1,13 @@
+package co.white.marsmall
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MarsmallApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

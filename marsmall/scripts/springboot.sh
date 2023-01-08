bfbe972bff0b9d9422bbd21da5075969d641afbe
Wordbe <seonghojin3@gmail.com>
2023-01-08 22:33:00 +0900

    fix: path

--- marsmall/scripts/springboot.sh
+++ marsmall/scripts/springboot.sh
@@ -3,7 +3,7 @@
 PROJECT=marsmall
 REPO="$HOME/actions-runner/_work/$PROJECT/$PROJECT"
 LOG_PATH="$HOME/$PROJECT/$PROJECT.log"
-PROFILE=dev
+PROFILE=prod
 
 CURR_PID=$(pgrep -f ${PROJECT}.*.jar)
 if [ -n "$CURR_PID" ]; then
@@ -14,10 +14,10 @@ fi
 
 # extract the largest size jar
 JAR=$(ls -S $REPO/$PROJECT/build/libs/*.jar | head -n 1)
-JVM_OPTIONS="-Dspring.profiles.active=$PROFILE -Dspring.config.import=$HOME/$PROJECT/secret/config.yml"
+JVM_OPTIONS="-Dspring.config.import=file:$HOME/$PROJECT/secret/config.yml -Dspring.profiles.active=$PROFILE"
 
 echo "application start: $JAR"
-echo "java -jar ""$JVM_OPTIONS"" ""$JAR"" > ""$LOG_PATH"" 2>&1 &"
+echo "nohup java -jar ""$JVM_OPTIONS"" ""$JAR"" > ""$LOG_PATH"" 2>&1 &"
 echo "log path: $LOG_PATH"
 
 # When nohup errors, output errors to log file

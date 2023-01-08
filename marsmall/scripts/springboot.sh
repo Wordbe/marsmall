a377e44c19e80914938d24768822dfad43aa7153
Wordbe <seonghojin3@gmail.com>
2023-01-08 22:53:26 +0900

    fix: double quotation modified

--- marsmall/scripts/springboot.sh
+++ marsmall/scripts/springboot.sh
@@ -3,6 +3,7 @@
 PROJECT=marsmall
 REPO="$HOME/actions-runner/_work/$PROJECT/$PROJECT"
 LOG_PATH="$HOME/$PROJECT/$PROJECT.log"
+PROFILE=prod
 
 CURR_PID=$(pgrep -f ${PROJECT}.*.jar)
 if [ -n "$CURR_PID" ]; then
@@ -13,12 +14,11 @@ fi
 
 # extract the largest size jar
 JAR=$(ls -S $REPO/$PROJECT/build/libs/*.jar | head -n 1)
-PROFILES="-Dspring.profiles.active=prod"
-SECRETS="-Dspring.config.import=file:$HOME/$PROJECT/secret/config.yml"
+JVM_OPTIONS="-Dspring.profiles.active=$PROFILE -Dspring.config.import=file:$HOME/$PROJECT/secret/config.yml"
 
 echo "application start: $JAR"
-echo "nohup java -jar ""$PROFILES"" ""$SECRETS"" ""$JAR"" > ""$LOG_PATH"" 2>&1 &"
+echo "nohup "java -jar "$JVM_OPTIONS" "$JAR"" > ""$LOG_PATH"" 2>&1 &"
 echo "log path: $LOG_PATH"
 
 # When nohup errors, output errors to log file
-nohup java -jar "$PROFILES" "$SECRETS" "$JAR" > "$LOG_PATH" 2>&1 &
+nohup "java -jar $JVM_OPTIONS $JAR" > "$LOG_PATH" 2>&1 &
